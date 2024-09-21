676ccbe89dda6f92fc35bbd729af632d92a06e37
Inazer <nazarromaniv671@gmail.com>
2024-09-21 12:48:06 +0300

    main

--- Car.cpp
+++ Car.cpp
@@ -25,18 +25,3 @@ bool Car::operator>(const Car& car2) const
 	else
 		return false;
 }
-
-
-
-int main()
-{
-	Car car2(50, 1500, 6, gasoline, 4, 300);
-	Car car1 = {};
-	if (car1 > car2) {//чи потужніша машина 1 за машину 2
-		cout << "The first car is more powerful than the second";
-	}
-	else
-		cout << "The second machine is more powerful than the first";
-}
-
-
