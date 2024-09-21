b79bcf642c3605347e1c23e3bc0ffb27f7da487c
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-21 11:04:14 +0300

    Class Car

---
+++ Car.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+class Car {
+private:
+	double engineVolume;//об'єм двигуна
+	int weight;//маса
+	int pistonCount;//поршні
+	//enum
+	int sitCount;//кі-сть мість
+	int tankVolume;//об'єм бака
+
+};
