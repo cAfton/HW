7ee0b8f5d68dde7ff86f894eaea0a36b7e4ffc80
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-21 13:13:39 +0300

    Check

--- 2109_1.cpp
+++ 2109_1.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 int main()
 {
+	Car car2(50, 1500, 6, gasoline, 4, 300);
+	Car car1 = {};
+	if (car1 > car2) {//чи потужніша машина 1 за машину 2
+		cout << "The first car is more powerful than the second";
+	}
+	else
+		cout << "The second machine is more powerful than the first";
 	CoffeeMachine coffeeMachine(true, 200, 500, 500, 250);
 
 	coffeeMachine.createCoffee(Latte);
