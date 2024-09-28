cef97e5d4a6279cdee377053e111ea45bae7beff
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-28 14:57:10 +0300

    Shota

--- 2809/2809/2809.cpp
+++ 2809/2809/2809.cpp
@@ -1,9 +1,77 @@
-#include "Array.h"
-#include "String.h"
-#include "UniversalArray.h"
+//#include "Array.h"
+//#include "String.h"
+//#include "UniversalArray.h"
+#include <iostream>
+
+using namespace std;
+
+
+template <typename T>
+T findA(T arr[], int size) {
+	T S = 0;//bad
+	for (int i = 0; i < size; i++)
+	{
+		S += arr[i];
+	}
+	return S / size;
+
+}
+
+template <typename T>
+T calc(T a, T b) {
+	return (- 1 * b) / a;
+}
+
+template <typename T>
+T calc(T a, T b, T c, bool first) {
+	T D = pow(b, 2) - 4 * a * c;
+	if (D < 0)
+		return -1;
+	else if (D = 0)
+		return (-1 * b) / (2 * a);
+	else {
+		if (first)//genius!
+			return ((-1 * b) + sqrt(D)) / 2 * a;
+		else
+			return ((-1 * b) - sqrt(D)) / 2 * a;
+	}
+	
+}
+
+template <typename T>
+T Max(T a, T b) {
+	if (a > b)
+		return a;
+	else if (a < b)
+		return b;
+	else
+		return a;
+}
+
+template <typename T>
+T Min(T a, T b) {
+	if (a < b)
+		return a;
+	else if (a > b)
+		return b;
+	else
+		return a;
+}
 
 int main()
 {
+
+
+	int arr1[5]{ 1,2,3,4,5 };
+	float arr2[5]{ 1.2,4.2,1.2,4.6,7.4 };
+	cout << findA(arr1, 5) << '\n' << findA(arr2, 5) << endl;
+
+	cout << calc(2, 4) << '\n' << calc(1, 2, 4, true) << '\t' << calc(1, 2, 4, false) << endl;
+
+	cout << "Max:" << Max(2, 5) << "\nMin:" << Min(3, 5);
+}
+
+	/*
 	int* numbers = new int[5] {1, 2, 3, 4, 5};
 	Array arr(numbers, 5 , 7);
 
@@ -81,5 +149,7 @@ int main()
 	CharUwwwU.Sort(true);
 
 	cout << CharUwwwU << endl;
+	*/
 
-}
+	
+
