ffdb86ab5f5bc66257721ac53f17bd86813bc999
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-28 12:20:05 +0300

    para2(tipa done)

--- 2809/2809/2809.cpp
+++ 2809/2809/2809.cpp
@@ -2,7 +2,32 @@
 
 int main()
 {
- 
-	int a = 5;
-	int* b = &a;
+	int* numbers = new int[5] {1, 2, 3, 4, 5};
+	Array arr(numbers, 5 , 7);
+
+	cout << arr << endl;
+
+	try {
+		arr.deleteByPosition(2);
+	}
+	catch (invalid_argument exept) {
+		cout << exept.what() << endl;
+	}
+
+	cout << arr << endl;
+	
+	arr.addByPosition(100, 2);
+	arr.addByPosition(110, 2);
+	arr.addByPosition(120, 2);
+
+	cout << arr << endl;
+
+	arr.Sort(true);
+	arr.Sort(false);
+
+	cout << arr << endl;
+
+	arr.Fit();
+
+	cout << arr;
 }
--- 2809/2809/Array.h
+++ 2809/2809/Array.h
@@ -9,13 +9,13 @@ private:
 	int size;
 	int length;
 
-	bool checkPosition(int position, bool isAdding);
+	bool checkPosition(int position, bool isAdding) const;
 public:
 	Array();
 	Array(int size);
 	Array(int* arr, int length, int size);
 	Array(const Array&);
-	Array(const Array&&);
+	Array(Array&&);
 
 	int getSize();
 	int getLength();
@@ -26,6 +26,8 @@ public:
 	void Fit();
 
 	void Sort(bool byDecreasing);//за спаданням
+
+	int& operator[](int index) const;
 
 	friend ostream& operator<<(ostream& out, const Array& Arr);
 	friend istream& operator>>(istream& in, Array& arr);
