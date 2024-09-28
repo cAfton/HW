9e0eb83513bad8a21f47944f59f86cd5db6a9045
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-28 10:30:16 +0300

    first para

---
+++ 2809/2809/Array.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Array {
+private:
+	int* arr;
+	int size;
+	int length;
+
+	bool checkPosition(int position, bool isAdding);
+public:
+	Array();
+	Array(int size);
+	Array(int* arr, int length, int size);
+	Array(const Array&);
+	Array(const Array&&);
+
+	int getSize();
+	int getLength();
+
+	void deleteByPosition(int position);
+	void addByPosition(int element, int position);
+
+	void Fit();
+
+	void Sort(bool byDecreasing);//за спаданням
+
+	friend ostream& operator<<(ostream& out, const Array& Arr);
+	friend istream& operator>>(istream& in, Array& arr);
+
+	~Array();
+};
