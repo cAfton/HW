dda1605286bbe15a958822a2b074f34c293f1234
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-30 19:13:17 +0300

    constructors

--- homeworks/Homework2/Date.h
+++ homeworks/Homework2/Date.h
@@ -9,6 +9,19 @@ private:
 	int month;
 	int year;
 public:
+	Date() {
+		this->day = 1;
+		this->month = 1;
+		this->year = 1970;
+	}
+	Date(int day, int month, int year) {
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	}
+
+
+
 	int Day() {
 		return this->day;
 	}
