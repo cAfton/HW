03cfcea449cf63b80914d795a00f424a5dac2697
cAfton <166216153+cAfton@users.noreply.github.com>
2024-09-30 19:23:27 +0300

    function adding days

--- homeworks/Homework2/Date.h
+++ homeworks/Homework2/Date.h
@@ -20,8 +20,6 @@ public:
 		this->year = year;
 	}
 
-
-
 	int Day() {
 		return this->day;
 	}
@@ -40,5 +38,135 @@ public:
 	}
 	void Year(int year) {
 		this->year = year;
+	}
+
+	void addDay() {
+		switch (this->month)
+		{
+		case 1:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 2:
+			if (this->day == 28)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 3:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 4:
+			if (this->day == 30)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 5:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 6:
+			if (this->day == 30)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 7:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 8:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 9:
+			if (this->day == 30)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 10:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 11:
+			if (this->day == 30)
+			{
+				this->day = 1;
+				this->month++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		case 12:
+			if (this->day == 31)
+			{
+				this->day = 1;
+				this->month = 1;
+				this->year++;
+			}
+			else {
+				this->day++;
+			}
+			break;
+		default:
+			cout << "month went over" << endl;
+			break;
+		}
 	}
 };
