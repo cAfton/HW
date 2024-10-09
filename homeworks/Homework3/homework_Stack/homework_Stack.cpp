c4f71129c8b9e49ad79e21dd4c7be0a4a1005f40
cAfton <166216153+cAfton@users.noreply.github.com>
2024-10-09 23:30:15 +0300

    try1

--- homeworks/Homework3/homework_Stack/homework_Stack.cpp
+++ homeworks/Homework3/homework_Stack/homework_Stack.cpp
@@ -1,8 +1,83 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+template<typename T>
+class Stack {
+    T* arr;
+    int lengh;
+    int size;
+public:
+    Stack() {
+        arr = new T[10];
+        lengh = 0;
+        size = 10;
+    }
+
+    void push(T elem) {
+        if (lengh == size)
+        {
+            T* newArr = new T[size * 2];
+            size *= 2;
+            for (int i = 0; i < lengh; ++i)
+            {
+                newArr[i] = arr[i];
+            }
+            newArr[lengh] = elem;
+            lengh++;
+            delete[] arr;
+            arr = newArr;
+        }
+        else {
+            arr[lengh] = elem;
+            lengh++;
+
+        }
+    }
+
+    T pop() {
+        if (lengh != 0)
+        {
+            lengh--;
+            T temp = arr[lengh];
+            return temp;
+        }
+
+    }
+
+    T peek() {
+        return arr[lengh - 1];
+    }
+};
+
 int main()
 {
-    cout << "Hello World!\n";
+    Stack<char> stack;
+    string text;
+    
+    getline(cin, text);
+
+    for (size_t i = 0; i < text.size(); i++)
+    {
+        cout << text[i];
+        if (text[i] == '(' || text[i] == '[' || text[i] == '{' || text[i] == '<')
+        {
+            stack.push(text[i]);
+        }
+
+        if (text[i] == ')' || text[i] == ']' || text[i] == '}' || text[i] == '>')
+        {
+            cout << "  !!! " << stack.peek() << " !!!   ";
+            if (stack.peek() != text[i])
+            {
+                cout << "<-- here is error" << endl;
+                break;
+            }
+            else {
+                stack.pop();
+            }
+        }
+    }
+
 }
