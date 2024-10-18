4e217cebaeb697c5396e59a95f0eb0488249f980
Inazer <nazarromaniv671@gmail.com>
2024-10-18 21:14:38 +0300

    Printer start

---
+++ homeworks/Homework3/homework_Queue/printer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Printer {
+
+	int* p;
+	string* array;
+
+	string* user;
+	int time;
+
+	int length;
+	int size;
+
+public:
+	void PrintAll();
+
+	Printer();
+
+};
