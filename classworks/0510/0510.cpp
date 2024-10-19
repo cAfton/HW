08a6ee071a870992fb7162afbb46041479024511
cAfton <166216153+cAfton@users.noreply.github.com>
2024-10-19 21:39:16 +0300

    f

--- classworks/0510/0510.cpp
+++ classworks/0510/0510.cpp
@@ -41,6 +41,7 @@ public:
             if (first != nullptr)
             {
                 Node* temp = first; 
+                
                 for (size_t i = 0; i < index; i++)
                 {
                     if (temp->next != nullptr)
@@ -50,10 +51,11 @@ public:
                     else {
                         
                         temp->next = new Node(elem, nullptr);
+                        return;
                     }
                 }
                 Node* insert = new Node(elem, temp->next);
-                temp->next = &insert;
+                temp->next = insert;
             }
             else {
                 first = new Node(elem, nullptr);
@@ -64,7 +66,7 @@ public:
     void showList() {
         Node* cur = first;
         while (cur != nullptr) {
-            cout << cur->item << endl;
+            cout << cur->item << " -> ";
             cur = cur->next;
         }
         delete cur;
@@ -78,8 +80,11 @@ int main()
 
     list1.Add(1, 0);
     list1.Add(2, 1);
+    list1.Add(12, 0);
     list1.Add(6, 2);
     list1.Add(3, 3);
+    list1.Add(16, 3);
+    list1.Add(13, 4);
     list1.showList();
 
 }
