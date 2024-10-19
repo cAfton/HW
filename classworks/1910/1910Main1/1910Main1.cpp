61b3aa356255c4e18bc308651af9dea7d475329f
cAfton <166216153+cAfton@users.noreply.github.com>
2024-10-19 14:07:04 +0300

    find

--- classworks/1910/1910Main1/1910Main1.cpp
+++ classworks/1910/1910Main1/1910Main1.cpp
@@ -3,6 +3,111 @@
 
 using namespace std;
 
+template<typename T>
+class Stack {
+    T* arr;
+    int lengh;
+    int size;
+public:
+    Stack() {
+        arr = new T[10];
+        lengh = 0;
+        size = 10;
+    }
+
+    void push(T elem) {
+            T* newArr = new T[size + 1];
+            size += 1;
+            for (int i = 0; i < lengh; ++i)
+            {
+                newArr[i] = arr[i];
+            }
+            newArr[lengh] = elem;
+            lengh++;
+            delete[] arr;
+            arr = newArr;
+           // cout << "lenght: " << lengh << endl;
+            //cout << "element: " << elem << endl;
+
+    }
+
+    T pop() {
+        if (lengh != 0)
+        {
+            T temp = arr[lengh - 1];
+            lengh--;
+            return temp;
+        }
+
+    }
+
+    T peek() {
+        return arr[lengh - 1];
+    }
+
+    bool isEmpty() {
+        if (lengh == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int Size() {
+        return this->lengh;
+    }
+
+    int Lengh() {
+        return this->lengh;
+    }
+};
+
+//
+//void reverce(Stack<int> stack) {
+//    if (stack.Lengh() > 0)
+//    {
+//        int* temp = new int[stack.Lengh()];
+//        int templengh = stack.Lengh();
+//        for (size_t i = 0; i < stack.Lengh() + 1; i++)
+//        {
+//            cout << "reverce peek pop: " << stack.peek() << endl;
+//            temp[i] = stack.pop();
+//        }
+//
+//        for (size_t i = 0; i < templengh; i++)
+//        {
+//            cout << "reverce peek push: " << stack.peek() << endl;
+//
+//            stack.push(temp[i]);
+//        }
+//    }
+//
+//}
+
+
+Stack<int> MultiplyStackByNum(int num, Stack<int>& stack) {
+    Stack<int> stackFinal;
+    int temp = 0;
+    while (!stack.isEmpty())
+    {
+
+        cout << "counted without temp: " << stack.peek() * num << endl;
+        cout << "counted with temp: " << (stack.peek() * num) + temp << endl;
+        int val = (stack.pop() * num) + temp;
+        cout << "val: " << val << endl;
+        temp = val / 10;
+        cout << "temp: " << temp << endl;
+        int add = val % 10;
+        cout << "add: " << add << endl;
+        stackFinal.push(add);
+        cout << "--------" << endl;
+
+    }
+    stackFinal.push(temp);
+
+    return stackFinal;
+}
+
 int recurciveFact(int num) {
 	if (num == 1)
 	{
@@ -32,9 +137,91 @@ int subFactorial(int numSub) {
 }
 // (n - 1) * (!(n -1) + !(n -2))
 
+Stack<int> factorialStack(int num, Stack<int> stackStart, Stack<int> stackFinal, bool once) {
+    cout << "size: " << stackStart.Size() << endl;
+    cout << "num: " << num << endl;
+    int val = 0;
+    int temp = 0;
+
+    if (once)
+    {
+        int first = (num * (num - 1)) % 10;
+        int second = (num * (num - 1)) / 10;
+        cout << "first: " << first << " || second:" << second << endl;
+        stackStart.push(first);
+        stackStart.push(second);
+        cout << "peek: " << stackStart.peek() << endl;
+    }
+    if (num == 1)
+    {
+        stackFinal.push(1);
+        return stackFinal;
+    }
+    cout << "\n\n\nwhile: " << endl; 
+    //reverce(stackStart);
+    //cout << "peek after reverce: " << stackStart.peek() << endl;
+
+    if (!once)
+    {
+        stackFinal = MultiplyStackByNum(num, stackStart);
+    }
+   
+    if (once)
+    {
+        stackFinal = stackStart;
+    }
+    once = false;
+
+    Stack<int> stackNew;
+
+    cout << endl;
+    cout << endl;
+
+    cout << "size: " << stackFinal.Size() << endl;
+    Stack<int> tempStack = stackFinal;
+    cout << "result: " << endl;
+    while (!tempStack.isEmpty()) {
+        cout << tempStack.pop();
+    }
+    cout << endl;
+
+    return factorialStack(num - 1, stackFinal, stackNew, once);
+}
+
+
+
 int main()
 {
+    Stack<int> stackStart;
+    Stack<int> stackFinal;
 	cout << recurciveFact(5) << endl;
 	cout << subFactorial(5) << endl;
-	cout << stepin(5, 3);
+	cout << stepin(5, 3) << endl;
+    Stack<int> stackFinalRes = factorialStack(5, stackStart, stackFinal, true);
+
+
+    cout << "////////////////////////////////////////////////" << endl;
+    while (!stackFinalRes.isEmpty()) {
+        cout << stackFinalRes.pop();
+    }
+    cout << endl;
+    cout << "////////////////////////////////////////////////" << endl;
+    Stack<int> tryStack;
+    
+    tryStack.push(3);
+    cout << "first: " << tryStack.peek() << endl;
+    tryStack.push(7);
+    cout << "second: " << tryStack.peek() << endl;
+    cout << "multiple by 5" << endl;
+    
+    //reverce(tryStack);
+    //cout << "after reversed: " << tryStack.peek() << endl;
+
+    Stack<int> printRes = MultiplyStackByNum(5, tryStack);
+
+    for (size_t i = 0;  !printRes.isEmpty(); i++)
+    {
+        cout << printRes.pop();
+    }cout << endl;
+
 }
---
+++ classworks/1910/1910Tree/1910Tree.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Tree
+
+{
+
+private:
+
+	struct Node
+
+	{
+		int elem;
+		Node* left;
+		Node* right;
+		Node()
+		{
+			elem = 0;
+			left = nullptr;
+			right = nullptr;
+		}
+		Node(int elem, Node* left, Node* right)
+		{
+			this->elem = elem;
+			this->left = left;
+			this->right = right;
+		}
+		friend ostream& operator<<(ostream& A, Node Elem)
+		{
+			A << Elem.elem;
+			return A;
+		}
+		void Add(int elem)
+		{
+			if (elem < this->elem)
+			{
+				if (this->left == nullptr) {
+					left = new Node(elem, nullptr, nullptr);
+				}
+				else
+				{
+					this->left->Add(elem);
+				}
+			}
+			else
+			{
+				if (this->right == nullptr) {
+					right = new Node(elem, nullptr, nullptr);
+				}
+				else
+				{
+					this->right->Add(elem);
+				}
+			}
+		}
+
+		void print(ostream& out) {
+			if (this->left != nullptr) {
+				left->print(out);
+				if (this->right != nullptr) {
+					right->print(out);
+					out << this->elem << ' ';
+				}
+				else {
+					out << this->elem << ' ';
+				}
+			}
+			else if (this->right != nullptr) {
+				right->print(out);
+				out << this->elem << ' ';
+			}
+			else {
+				out << this->elem << ' ';
+			}
+		}
+		
+		int find(int numToFind) {
+			if (numToFind > this->elem)
+			{
+				if (this->right != nullptr)
+				{
+					right->find(numToFind);
+				}
+				else {
+					throw invalid_argument("error");
+				}
+			}
+			else if (numToFind < this->elem)
+			{
+				if (this->left != nullptr)
+				{
+					left->find(numToFind);
+				}
+				else {
+					throw invalid_argument("error");
+				}
+			}
+			else {
+				return this->elem;
+			}
+		}
+	};
+
+	Node* Root;
+
+
+public:
+
+	void Add(int elem)
+	{
+		if (Root == nullptr)
+		{
+			Root = new Node(elem, nullptr, nullptr);
+		}
+		else
+		{
+			Root->Add(elem);
+		}
+	}
+
+	int find(int numToFind) {
+		if (Root == nullptr)
+		{
+			throw invalid_argument("error");
+		}
+		else if (Root->elem == numToFind)
+		{
+			return Root->elem;
+		}
+		else
+		{
+			Root->find(numToFind);
+		}
+	}
+
+	friend ostream& operator<<(ostream& out, Tree copy) {
+		copy.Root->print(out);
+		return out;
+	}
+
+	Tree() {
+		Root = nullptr;
+	}
+
+	~Tree()
+	{
+
+	}
+
+};
+
+
+int main() {
+	Tree newTree1;
+	newTree1.Add(8);
+	newTree1.Add(2);
+	newTree1.Add(10);
+	newTree1.Add(6);
+	newTree1.Add(1);
+	newTree1.Add(-4);
+	newTree1.Add(5);
+	newTree1.Add(-8);
+	newTree1.Add(4);
+	newTree1.Add(7);
+	newTree1.Add(12);
+	newTree1.Add(16);
+	newTree1.Add(11);
+
+
+	cout << newTree1 << endl;
+
+	try {
+		cout << newTree1.find(-8) << endl;
+	}
+	catch (invalid_argument) {
+		cout << "error" << endl;
+	}
+}
