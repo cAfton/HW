c3e4f034708df50ed3d888864e0fed2ef0f7ce13
cAfton <166216153+cAfton@users.noreply.github.com>
2024-10-27 16:12:04 +0200

    Car, Tree

--- homeworks/Homework5/TreeDAI/TreeDAI.cpp
+++ homeworks/Homework5/TreeDAI/TreeDAI.cpp
@@ -1,20 +1,223 @@
-// TreeDAI.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+using namespace std;
+
+enum Offenses
 {
-    std::cout << "Hello World!\n";
+	speeding,
+	illegal_parking,
+	running_a_red_light,
+	no_seatbelt,
+	phone_use_while_driving,
+};
+
+class Car {
+	int carNumber;
+	Offenses* offense;
+	int size;
+
+public:
+	Car() {
+		this->carNumber = 1000;
+		this->size = 0;
+		this->offense = new Offenses[size];
+	}
+
+	Car(int number, int size, Offenses* offense) {
+		this->carNumber = number;
+		this->size = size;
+		this->offense = offense;
+	}
+
+	Car(int number) {
+		this->carNumber = number;
+		this->size = 0;
+		this->offense = new Offenses[size];
+	}
+
+
+	int getNumber() {
+		return this->size;
+	}
+
+	void addOffense(Offenses newOffense) {
+		Offenses* temp = new Offenses[size + 1];
+		for (size_t i = 0; i < size; i++)
+		{
+			temp[i] = offense[i];
+		}
+		temp[size] = newOffense;
+		delete[] offense;
+		offense = temp;
+		size++;
+	}
+
+	friend ostream& operator<<(ostream& out, Car car) {
+		out << "\nCar number: " << car.carNumber << endl;
+		out << "Offenses: \n";
+		for (size_t i = 0; i < car.size; i++)
+		{
+			out << car.offense[i] << endl;
+		}
+		out << "\nTotal " << car.size << " offenses" << endl;
+	}
+};
+
+
+class Tree
+{
+private:
+	struct Node
+	{
+		Car elem;
+		Node* left;
+		Node* right;
+
+		Node()
+		{
+			left = nullptr;
+			right = nullptr;
+		}
+
+		Node(Car elem, Node* left, Node* right)
+		{
+			this->elem = elem;
+			this->left = left;
+			this->right = right;
+		}
+
+		friend ostream& operator<<(ostream& A, Node Elem)
+		{
+			A << Elem.elem;
+			return A;
+		}
+
+		void Add(Car elem)
+		{
+			if (elem.getNumber() < this->elem.getNumber())
+			{
+				if (this->left == nullptr) {
+					left = new Node(elem, nullptr, nullptr);
+				}
+				else
+				{
+					this->left->Add(elem);
+				}
+			}
+			else
+			{
+				if (this->right == nullptr) {
+					right = new Node(elem, nullptr, nullptr);
+				}
+				else
+				{
+					this->right->Add(elem);
+				}
+			}
+		}
+
+		void print(ostream& out) {
+			if (this->left != nullptr) {
+				left->print(out);
+				if (this->right != nullptr) {
+					right->print(out);
+					out << this->elem << ' ';
+				}
+				else {
+					out << this->elem << ' ';
+				}
+			}
+			else if (this->right != nullptr) {
+				right->print(out);
+				out << this->elem << ' ';
+			}
+			else {
+				out << this->elem << ' ';
+			}
+		}
+
+		Car find(int numToFind) {
+			if (numToFind > this->elem.getNumber())
+			{
+				if (this->right != nullptr)
+				{
+					return right->find(numToFind);
+				}
+				else {
+					throw invalid_argument("error");
+				}
+			}
+			else if (numToFind < this->elem.getNumber())
+			{
+				if (this->left != nullptr)
+				{
+					return left->find(numToFind);
+				}
+				else {
+					throw invalid_argument("error");
+				}
+			}
+			else {
+				return this->elem;
+			}
+		}
+	};
+
+	Node* Root;
+
+
+public:
+
+	void Add(Car elem)
+	{
+		if (Root == nullptr)
+		{
+			Root = new Node(elem, nullptr, nullptr);
+		}
+		else
+		{
+			Root->Add(elem);
+		}
+	}
+
+	Car find(int numToFind) {
+		if (Root == nullptr)
+		{
+			throw invalid_argument("error");
+		}
+		else if (Root->elem.getNumber() == numToFind)
+		{
+			return Root->elem;
+		}
+		else
+		{
+			Root->find(numToFind);
+		}
+	}
+
+	friend ostream& operator<<(ostream& out, Tree copy) {
+		copy.Root->print(out);
+		return out;
+	}
+
+	Tree() {
+		Root = nullptr;
+	}
+
+
+};
+
+
+int main() {
+	//Tree DAI;
+	int userChoise = -1;
+
+	while (userChoise == 0)
+	{
+		cout << "0 - exit\n\n1 - add fine receipt\n2 - print all receipts\n3 - print receipt by number\n4 - print by number range\n\n";
+		cin >> userChoise;
+	}
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
