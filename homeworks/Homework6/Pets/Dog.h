ef45e66d5806b27011ab632f70bcea65c5783609
cAfton <166216153+cAfton@users.noreply.github.com>
2024-10-27 17:36:20 +0200

    class Dog

--- homeworks/Homework6/Pets/Dog.h
+++ homeworks/Homework6/Pets/Dog.h
@@ -1,3 +1,34 @@
 #pragma once
 #include "Pet.h"
 
+#pragma once
+#include "Pet.h"
+
+class Dog : public Pet {
+	int guarding_qualities; //dog's ability to guard its owners
+public:
+	Dog() : Pet() {
+		this->guarding_qualities = 0;
+	}
+
+	Dog(string name, int age, int guarding_qualities) : Pet(name, age) {
+		this->guarding_qualities = guarding_qualities;
+	}
+
+	void Sleep(int hours) {
+		cout << "Your dog has slept for " << hours << " hours" << endl;
+	}
+
+	void Eat(string food) {
+		cout << "Your dog eat " << food << endl;
+	}
+
+	int Intelligence() {
+		return getAge() * guarding_qualities * 2; //let it be
+	}
+
+	friend ostream& operator<<(ostream& out, Dog& dog) {
+		out << static_cast<Pet&>(dog) << "\nCan guard you: " << dog.guarding_qualities << "/10";
+		return out;
+	}
+};
