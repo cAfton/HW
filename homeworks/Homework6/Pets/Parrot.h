69314fe1751ff3ef0a8229984674502fc38eca71
cAfton <166216153+cAfton@users.noreply.github.com>
2024-10-27 17:32:24 +0200

    Parrot class

--- homeworks/Homework6/Pets/Parrot.h
+++ homeworks/Homework6/Pets/Parrot.h
@@ -1,2 +1,31 @@
 #pragma once
 #include "Pet.h"
+
+class Parrot : public Pet {
+	int words; // how many words know
+public:
+	Parrot() : Pet() {
+		this->words = 0;
+	}
+
+	Parrot(string name, int age, int words) : Pet(name, age) {
+		this->words = words;
+	}
+
+	void Sleep(int hours) {
+		cout << "Your parrot has slept for " << hours << " hours" << endl;
+	}
+
+	void Eat(string food) {
+		cout << "Your parrot eat " << food << endl;
+	}
+
+	int Intelligence() {
+		return getAge() * words; //let it be
+	}
+
+	friend ostream& operator<<(ostream& out, Parrot& parrot) {
+		out << static_cast<Pet&>(parrot) << "\nKnow " << parrot.words << " words";
+		return out;
+	}
+};
--- homeworks/Homework6/Pets/Pet.h
+++ homeworks/Homework6/Pets/Pet.h
@@ -9,7 +9,28 @@ class Pet
 	int age;
 public:
 	Pet() {
+		this->name = "a cool name";
+		this->age = 1;
+	}
 
+	Pet(string name, int age) {
+		this->name = name;
+		this->age = age;
+	}
+
+	int getAge() {
+		return this->age;
+	}
+
+	virtual void Sleep(int hours) = 0;
+
+	virtual void Eat(string food) = 0;
+
+	virtual int Intelligence() = 0;
+
+	friend ostream& operator<<(ostream& out, Pet& pet) {
+		out << "Name: " << pet.name << "\nAge: " << pet.age;
+		return out;
 	}
 
 };
