a52bce805019c2ef605d0b4d05f02417c94a4fad
cAfton <166216153+cAfton@users.noreply.github.com>
2024-11-07 22:08:41 +0200

    step1

--- homeworks/Homework7/VirtualFunction/VirtualFunction.cpp
+++ homeworks/Homework7/VirtualFunction/VirtualFunction.cpp
@@ -1,20 +1,43 @@
-// VirtualFunction.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+class File {
+    ofstream filesName;
+
+public:
+    File(string path) {
+        this->filesName.open(path);
+
+        if (!this->filesName.is_open())
+        {
+            throw runtime_error("");
+        }
+    }
+
+    virtual void Display(string path) = 0;
+
+    ~File() {
+        if (this->filesName.is_open()) {
+            this->filesName.close();
+        }
+    }
+
+};
+
+class FIleASCII : public File {
+public:
+    FIleASCII(string path) : File(path) {}
+
+    void Display(string path) {
+        cout << 
+    }
+};
 
 int main()
 {
+    
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
