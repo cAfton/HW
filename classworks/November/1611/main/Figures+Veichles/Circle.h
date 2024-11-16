fbf3792f3c38921bf03e307870356c8e5d1dd8c9
cAfton <166216153+cAfton@users.noreply.github.com>
2024-11-16 09:10:29 +0200

    figures

---
+++ classworks/November/1611/main/Figures+Veichles/Circle.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "Figures.h"
+
+
+class Circle : public Figures {
+private:
+	int R;
+	int X;
+	int Y;
+public:
+	Circle() {
+		this->R = 1;
+		this->X = 0;
+		this->Y = 0;
+	}
+
+	Circle(int r, int x, int y) {
+		this->R = r;
+		this->X = x;
+		this->Y = y;
+	}
+	string Print() {
+		string ret = "Circle: " + to_string(this->R) + "\tCordinates (" + to_string(this->X) + "; " + to_string(this->Y) + ")";
+		return ret;
+
+	}
+
+	void Save(string path) {
+		ofstream file(path, ios::trunc);
+		if (!file.is_open())
+		{
+			throw runtime_error("coudn't open file");
+		}
+
+		file << this->R << endl;
+		file << this->X << endl;
+		file << this->Y << endl;
+
+		file.close();
+
+	}
+
+	void Load(string path) {
+		ifstream file(path);
+		if (!file.is_open())
+		{
+			throw runtime_error("coudn't open file");
+		}
+		string save;
+		getline(file, save);
+		this->R = stoi(save);
+
+		getline(file, save);
+		this->X = stoi(save);
+
+		getline(file, save);
+		this->Y = stoi(save);
+	}
+
+	float Area() {
+		return 3.14 * this->R * this->R;
+	
+	}
+
+	float Perimetr() {
+		return 2 * 3.14 * this->R;
+	}
+
+};
