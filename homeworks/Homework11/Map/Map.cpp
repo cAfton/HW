b2bee212885891bed9bd6fd61c032075fb823c32
cAfton <166216153+cAfton@users.noreply.github.com>
2024-12-02 21:47:00 +0200

    done

--- homeworks/Homework11/Map/Map.cpp
+++ homeworks/Homework11/Map/Map.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
-
-using namespace std;
+#include "Dictionary.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	string text = "The sun. was setting, painting .the sky in vibrant shades of orange and pink. The sun was warm, and the sun slowly disappeared behind the hills. The sky changed, and the sky became darker as the sun faded.";
+	Dictionary dict(text);
+
+	cout << dict << endl;
+
+	cout << endl;
+
+	cout << dict.theMostUsedWord();
+
+	cout << endl;
+	cout << endl;
+
+
+
 }
