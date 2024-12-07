ddc1cc383eab0eb76884feaff9d4b2bcd4600339
cAfton <166216153+cAfton@users.noreply.github.com>
2024-12-07 14:54:38 +0200

    para4(with errors)

--- classworks/December/0712/First Para/First para/First para.cpp
+++ classworks/December/0712/First Para/First para/First para.cpp
@@ -15,6 +15,15 @@ public:
 		this->mayor = mayor;
 	}
 
+	City(){}
+
+	string GetName() {
+		return this->name;
+	}
+	void setName(string newName) {
+		this->name = newName;
+	}
+
 	friend ostream& operator<<(ostream& out, City copy) {
 		out << "City`s name:" << copy.name << "\nMayor:" << copy.mayor << endl;
 		return out;
@@ -23,6 +32,19 @@ public:
 	friend ofstream& operator<<(ofstream& out, City& copy) {
 		out << copy.name << ';' << copy.mayor << ':';
 		return out;
+	}
+
+	friend istream& operator>>(istream& in, City& copy) {
+		cout << "Cities name: ";
+		in >> copy.name;
+		cout << "Cities Mayor: ";
+		in >> copy.mayor;
+
+		return in;
+	}
+
+	bool operator==(City& cit) {
+		return this->name == cit.GetName();
 	}
 };
 
@@ -35,6 +57,8 @@ public:
 		this->name = name;
 		this->president = president;
 	}
+
+	Country() {}
 
 	string GetName() {
 		return this->name;
@@ -56,6 +80,18 @@ public:
 		for_each(copy.cities.begin(), copy.cities.end(), [&out](City& city) {out << city; });
 		return out;
 	}
+
+	friend istream& operator>>(istream& in, Country& copy) {
+		cout << "Countries name: ";
+		in >> copy.name;
+		cout << "Countries President: ";
+		in >> copy.president;
+
+		return in;
+	}
+	bool operator==(Country& c) {
+		return this->name == c.GetName();
+	}
 };
 
 
@@ -69,28 +105,56 @@ void FindCities(vector<Country>& Countries) {
 	
 }
 
-void ChangeName(vector<string>& Countries) {
+void ChangeName(vector<Country>& Countries) {
 	string county;
-	cout << "Which city you`d like to change?";
+	cout << "In which country is city you`d like to change?";
 	cin >> county;
+
+	auto it = find_if(Countries.begin(), Countries.end(), [county](Country elem) {return county == elem.GetName(); });
+
+	string city;
+	cout << "\nEnter city's name to change: ";
+	cin >> city;
+
+	auto it2 = find_if(it->GetCities().begin(), it->GetCities().end(), [city](City elem) {return city == elem.GetName(); });
+
+	cout << "\nEnter new name: ";
+	cin >> city;
+
+	it2->setName(city);
 	
 }
 
-void AddCityOrCounry() {
+void AddCityOrCountry(vector<Country>& Countries) {
 	int a;
-	bool b;
-	cout << "what 1.countre or 2.city you want?" << endl;
+	cout << "what 1.country or 2.city you want?" << endl;
 	cin >> a;
 	string c;
 	if (a == 1) {
-		b = true;
-		cout << "enter countre name" << endl;
-		cin >> c;
+		Country newCountry;
+
+		cin >> newCountry;
+
+		Countries.push_back(newCountry);
 	}
 	else {
-		b = false;
-		cout << "enter city name" << endl;
-		cin >> c;
+		string country;
+		cout << "Enter country: ";
+		cin >> country;
+
+		auto it = find_if(Countries.begin(), Countries.end(), [country](Country count) {return country == count.GetName(); });
+
+		if (it != Countries.end())
+		{
+			City newCity;
+			cin >> newCity;
+
+			it->GetCities().push_back(newCity);
+		}
+		else {
+			cout << "Country does not exist" << endl;
+		}
+		
 	}
 }
 
@@ -106,32 +170,60 @@ void DisplayCities() {
 
 }
 
-void NumberOfCities(vector<string>) {
+void NumberOfCities(vector<Country>& Countries) {
 	string county;
-	cout << "Which counry`s number of cities you`d like to see?";
+	cout << "Which country`s number of cities you`d like to see?";
+
 	cin >> county;
+
+	auto it = find_if(Countries.begin(), Countries.end(), [county](Country country) {return county == country.GetName(); });
+	
+	cout << it->GetCities().size();
 }	
 
-void Delete(vector<string>) {
+void Delete(vector<Country>& Countries) {
 	string name;
 	int countyOrCity;
-	bool choice;
 	cout << "Which 1.counry or 2.city you`d like to delete?";
 	cin >> countyOrCity;
 	if (countyOrCity == 1)
 	{
-		choice = true;
 		cout << "Enter county name:";
 		cin >> name;
+		auto it = find_if(Countries.begin(), Countries.end(), [name](Country count) {return name == count.GetName(); });
 
-
+		if (it != Countries.end())
+		{
+			Country country();
+			remove(Countries.begin(), Countries.end(), *it);
+		}
+		else {
+			cout << "Country does not exist" << endl;
+		} 
 	}
 	else if (countyOrCity == 2)
 	{
-		choice = false;
-		cout << "Enter city name:";
-		cin >> name;
+		string country;
+		cout << "Enter country: ";
+		cin >> country;
 
+		auto it = find_if(Countries.begin(), Countries.end(), [country](Country elem) {return country == elem.GetName(); });
+		
+
+		if (it != Countries.end())
+		{
+			string city;
+			cout << "\nEnter city: ";
+			cin >> city;
+			City cityToDel(city, "");
+			auto it1 = find(it->GetCities().begin(), it->GetCities().end(), [city](City t) {return city == t.GetName(); });
+
+			it->GetCities().erase(*it1);
+			remove(it->GetCities().begin(), it->GetCities().end(), *it1);
+		}
+		else {
+			cout << "Country does not exist" << endl;
+		}
 	}
 	else
 	{
@@ -145,9 +237,9 @@ void readFromFile(string filepath, vector<Country>& Countries) {
 
 	while (getline(file,line))
 	{
-		int pos = line.find(';'); //ЎукаЇмо першу ";"
-		string name = line.substr(0, pos); //«бер≥гаЇмо частину тексту в≥д початку до ";"
-		line.erase(0, pos + 1); //¬идал€Їмо збережений р€док з початкового тексту
+		int pos = line.find(';');
+		string name = line.substr(0, pos);
+		line.erase(0, pos + 1);
 
 		pos = line.find(';');
 		string president = line.substr(0, pos);
@@ -193,46 +285,39 @@ int main()
 
 	readFromFile(filepath, Countries);
 
-	for_each(Countries.begin(), Countries.end(), [](Country country) {cout << country << endl; });
-
-
-
-	writeToFile(filepath, Countries);
-
-
-	cout << "STL\nList of cities" << endl;
-	int choice;
-	cout << "|1. Find city            |" << endl;//-
-	cout << "|2. Change cities name   |" << endl;//+
-	cout << "|3. Add city or counry   |" << endl;//+
-	cout << "|4. Delete city or counry|" << endl;//+
-	cout << "|5. Number of cities     |" << endl;//+
-	cout << "|6. Display counries     |" << endl;//-
-	cout << "|7. Display cities       |\n|   of one counry        |" << endl;//+
-	cout << "|0. Exit                 |" << endl;
-	cin >> choice;
+	
 	while (true)
 	{
+		cout << "STL\nList of cities" << endl;
+		int choice;
+		cout << "|1. Find city             |" << endl;//+
+		cout << "|2. Change cities name    |" << endl;//+
+		cout << "|3. Add city or country   |" << endl;//-
+		cout << "|4. Delete city or country|" << endl;//-
+		cout << "|5. Number of cities      |" << endl;//-
+		cout << "|6. Display countries     |" << endl;//-
+		cout << "|0. Exit                  |" << endl;//+
+		cin >> choice;
 		switch (choice) {
 		case 1:
 			FindCities(Countries);
 			break;
 
 		case 2:
-			//ChangeName(vector<string>);
+			ChangeName(Countries);
 			break;
 
 		case 3:
-			AddCityOrCounry();
+			AddCityOrCountry(Countries);
 			break;
 
 		case 4:
-			//Delete();
+			Delete(Countries);
 
 			break;
 
 		case 5:
-			//NumberOfCities(vector<string>);
+			NumberOfCities(Countries);
 			break;
 
 		case 6:
@@ -245,6 +330,7 @@ int main()
 			break;
 
 		case 0:
+			writeToFile(filepath, Countries);
 			return 0;
 			break;
 
